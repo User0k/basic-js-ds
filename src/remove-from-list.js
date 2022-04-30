2101dac7bb3b79cf1edf6b31f756026732cd1f49
User0k <glucus2@yandex.ru>
2022-04-30 21:06:44 +0300

    feat: implement remove from list with mistake in tail

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function removeKFromList(l, k) {
+  while (l.value === k) {
+    l = l.next;
+  };
+
+  if (l.next === null) return l;
+
+  l.next = removeKFromList(l.next, k);
+  return l;
 }
 
 module.exports = {
